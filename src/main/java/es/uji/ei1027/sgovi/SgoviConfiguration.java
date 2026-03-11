779e553e7f5e40ecdb9fbcd2cdb42b711c3fabd1
al387384 <al387384@uji.es>
2026-03-11 12:09:40 +0100

    Estructura inicial (clases Configuration + Application)

---
+++ src/main/java/es/uji/ei1027/sgovi/SgoviConfiguration.java
@@ -0,0 +1,19 @@
+package es.uji.ei1027.sgovi;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class SgoviConfiguration {
+    // Configura l'accés a la base de dades (DataSource)
+    // a partir de les propietats a src/main/resources/applications.properties
+    // que comencen pel prefix spring.datasource
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
