5199cd3bd50c732f4cb7a92b46351c10417d5af0
Alex-Doaga <al450630@uji.es>
2026-03-12 14:02:06 +0100

    OviUserDao

---
+++ src/main/java/es/uji/ei1027/sgovi/dao/OviUserDao.java
@@ -0,0 +1,75 @@
+package es.uji.ei1027.sgovi.dao;
+
+import es.uji.ei1027.sgovi.modelo.OviUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository // En Spring els DAOs van anotats amb @Repository
+public class OviUserDao {
+    private JdbcTemplate jdbcTemplate;
+
+    // Obté el jdbcTemplate a partir del Data Source
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    /* Añade un oviUser a la base de datos */
+    public void addOviUser(OviUser user) {
+        jdbcTemplate.update(
+                "INSERT INTO ovi_user (id_ovi_user, name, surname, dni_nie, birth_date, address, city, postal_code, email, phone, entity, name_tutor, dni_nie_tutor, has_depen_degree, depen_degree, project_life_doc, social_service_center) " +
+                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                user.getIdOviUser(), user.getName(), user.getSurname(), user.getDniNie(), user.getBirthDate(),
+                user.getAddress(), user.getCity(), user.getPostalCode(), user.getEmail(), user.getPhone(),
+                user.getEntity(), user.getNameTutor(), user.getDniNieTutor(), user.getHasDepenDegree(),
+                user.getDepenDegree(), user.getProjectLifeDoc(), user.getSocialServiceCenter()
+        );
+    }
+
+    /* Borra un oviUser de la base de datos */
+    public void deleteOviUser(int idOviUser) {
+        jdbcTemplate.update("DELETE FROM ovi_user WHERE id_ovi_user=?", idOviUser);
+    }
+
+    /* Actualiza los atributos del oviUser
+    (excepto el id, que es la clave primaria) */
+    public void updateOviUser(OviUser user) {
+        jdbcTemplate.update(
+                "UPDATE ovi_user SET name=?, surname=?, dni_nie=?, birth_date=?, address=?, city=?, " +
+                        "postal_code=?, email=?, phone=?, entity=?, name_tutor=?, dni_nie_tutor=?, " +
+                        "has_depen_degree=?, depen_degree=?, project_life_doc=?, social_service_center=? " +
+                        "WHERE id_ovi_user=?",
+                user.getName(), user.getSurname(), user.getDniNie(), user.getBirthDate(),
+                user.getAddress(), user.getCity(), user.getPostalCode(), user.getEmail(),
+                user.getPhone(), user.getEntity(), user.getNameTutor(), user.getDniNieTutor(),
+                user.getHasDepenDegree(), user.getDepenDegree(), user.getProjectLifeDoc(),
+                user.getSocialServiceCenter(),
+                user.getIdOviUser()
+        );
+    }
+
+    /* Obtiene el oviUser con el nombre dado. Devuelve null si no existe. */
+    public OviUser getOviUser(int idOviUser) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM ovi_user WHERE id_ovi_user=?",
+                    new OviUserRowMapper(), idOviUser);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    /* Obtiene todos los oviUsers. Devuelve una lista vacía si no hay oviUsers */
+    public List<OviUser> getOviUsers() {
+        try {
+            return jdbcTemplate.query("SELECT * FROM ovi_user",
+                    new OviUserRowMapper());
+        } catch (EmptyResultDataAccessException e) {
+            return new ArrayList<OviUser>();
+        }
+    }
+}
