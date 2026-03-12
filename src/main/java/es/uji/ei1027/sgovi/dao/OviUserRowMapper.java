cb169611d10aa81b899159bbd9ed137ee239e8b8
Alex-Doaga <al450630@uji.es>
2026-03-12 01:27:16 +0100

    RequestRowMapper

---
+++ src/main/java/es/uji/ei1027/sgovi/dao/OviUserRowMapper.java
@@ -0,0 +1,35 @@
+package es.uji.ei1027.sgovi.dao;
+
+import es.uji.ei1027.sgovi.modelo.OviUser;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public final class OviUserRowMapper implements RowMapper<OviUser> {
+
+    public OviUser mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        OviUser oviUser = new OviUser();
+        oviUser.setIdOviUser(rs.getInt("id_ovi_user"));
+        oviUser.setName(rs.getString("name"));
+        oviUser.setSurname(rs.getString("surname"));
+        oviUser.setDniNie(rs.getString("dni_nie"));
+        oviUser.setBirthDate(rs.getObject("birth_date", LocalDate.class));
+        oviUser.setAddress(rs.getString("address"));
+        oviUser.setCity(rs.getString("city"));
+        oviUser.setPostalCode(rs.getString("postal_code"));
+        oviUser.setEmail(rs.getString("email"));
+        oviUser.setPhone(rs.getString("phone"));
+        oviUser.setEntity(rs.getString("entity"));
+        oviUser.setNameTutor(rs.getString("name_tutor"));
+        oviUser.setDniNieTutor(rs.getString("dni_nie_tutor"));
+        oviUser.setHasDepenDegree(rs.getBoolean("has_depen_degree"));
+        oviUser.setDepenDegree(rs.getInt("depen_degree"));
+        oviUser.setProjectLifeDoc(rs.getString("project_life_doc"));
+        oviUser.setSocialServiceCenter(rs.getString("social_service_center"));
+        return oviUser;
+    }
+
+}
