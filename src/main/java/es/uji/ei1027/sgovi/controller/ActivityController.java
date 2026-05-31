1ecfaa57a7385b2ab4be5b36bd643e704e025b3b
Alexandru Godja Lepadat <al435145@uji.es>
2026-05-31 21:52:59 +0200

    activity modificado

--- src/main/java/es/uji/ei1027/sgovi/controller/ActivityController.java
+++ src/main/java/es/uji/ei1027/sgovi/controller/ActivityController.java
@@ -1,37 +1,85 @@
 package es.uji.ei1027.sgovi.controller;
 
 import es.uji.ei1027.sgovi.dao.ActivityDao;
-import es.uji.ei1027.sgovi.modelo.Activity;
-import es.uji.ei1027.sgovi.modelo.Instructor;
+import es.uji.ei1027.sgovi.dao.InscriptionDao;
+import es.uji.ei1027.sgovi.modelo.*;
 import es.uji.ei1027.sgovi.validadores.ActivityValidator;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import es.uji.ei1027.sgovi.dao.UserDao;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import es.uji.ei1027.sgovi.modelo.UserDetails;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/activity")
 public class ActivityController {
     private ActivityDao activityDao;
     private int pageLength = 5;
+    private UserDao userDao;
+    private InscriptionDao inscriptionDao;
 
     @Autowired
     public void setActivityDao(ActivityDao activityDao) {
         this.activityDao = activityDao;
     }
 
+    @Autowired
+    public void setInscriptionDao(InscriptionDao inscriptionDao) {
+        this.inscriptionDao = inscriptionDao;
+    }
+    @Autowired
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
     @RequestMapping("/list")
-    public String listActivities(Model model, @RequestParam("page") Optional<Integer> page){
+    public String listActivities(Model model, @RequestParam("page") Optional<Integer> page, HttpSession session) {
+        UserDetails user = (UserDetails) session.getAttribute("user");
+
         List<Activity> activities = activityDao.getActivities();
-        Paginador.paginate(model,activities,page,pageLength,"activitiesPaged");
+        Paginador.paginate(model, activities, page, pageLength, "activitiesPaged");
+
+        if ("Tecnico".equals(user.getRol())) {
+            return "activity//list-tecnico";
+        }
         return "activity/list";
+
+    }
+    @RequestMapping("/join/{id}")
+    public String joinActivity(@PathVariable int id, HttpSession session) {
+        UserDetails user = (UserDetails) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+        }
+
+        Inscription inscription = new Inscription();
+        inscription.setIdActivity(id);
+        inscription.setDate(LocalDate.now());
+
+
+        if ("OviUser".equals(user.getRol())) {
+
+            inscription.setIdOviUser(1);
+            inscription.setIdPa(null);
+        } else if ("PA".equals(user.getRol())) {
+            inscription.setIdPa(1);
+            inscription.setIdOviUser(null);
+        }
+
+        inscriptionDao.addInscription(inscription);
+        return "redirect:/activity/list";
     }
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add(Model model) {
